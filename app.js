9ccf4c94da6e66320fac2caa491c2515e09b4e41
温子 <pitchpomg101@gmail.com>
2019-12-05 14:50:10 +0900

    ログイン画面から投稿画面へ飛ぶところ

--- app.js
+++ app.js
@@ -10,21 +10,21 @@ app.use(express.static('training'));
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(bodyParser.json());
 
-app.get('/login', (req,res) => {
+app.get('/login', (req, res) => {
   console.log('login')
   res.sendFile(__dirname + '/login.html')
 });
 
-
-app.post('/post', (req, res) => {
-    console.log(req.body);
-res.render('/login', {title:'', id: req,body,id});
-    });
-
+app.post('/login', (req, res) => {
+  const id = req.body.user_id;
+  const password = req.body.password;
+  console.log(req.body);
+  res.sendFile(__dirname + '/bbs.html')
+});
 
 // ポート3000でサーバを立てる
 app.listen(port);
